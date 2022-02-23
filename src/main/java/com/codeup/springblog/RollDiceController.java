42e495ad3a4fbbf657ef58a94af25f2012c48296
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-02-23 11:15:49 -0600

    roll dice exercise

---
+++ src/main/java/com/codeup/springblog/RollDiceController.java
@@ -0,0 +1,31 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class RollDiceController {
+    @GetMapping("/roll-dice")
+    public String rolldice(){
+        return "dice/roll-dice";
+    }
+
+    @GetMapping("/roll-dice/{guess}")
+    public  String rollDiceGuess(@PathVariable int guess, Model model){
+        String message;
+        int random = (int) Math.ceil(Math.random() * 6);
+
+        if (random == guess) {
+            message = "You guessed the random number!";
+        } else {
+            message = "Sorry, please try again";
+        }
+        model.addAttribute("message", message);
+        model.addAttribute("guess", guess);
+        model.addAttribute("random", random);
+
+        return "dice/roll-dice-guess";
+    }
+}
