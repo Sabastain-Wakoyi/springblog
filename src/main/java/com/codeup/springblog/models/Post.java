20f7036f11b3f0dfb12b7f1fb457cb883e77cdd3
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-02-27 16:46:57 -0600

    updated the repositories and jpa exercises

--- src/main/java/com/codeup/springblog/models/Post.java
+++ src/main/java/com/codeup/springblog/models/Post.java
@@ -1,14 +1,12 @@
-package models;
+package com.codeup.springblog.models;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name="posts")
 public class Post  {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
