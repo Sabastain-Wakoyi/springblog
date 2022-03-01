e6aaf42a4c018c15aebe24e1835f9d097741de45
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-03-01 11:51:30 -0600

    form model exercise

--- src/main/java/com/codeup/springblog/controllers/BookController.java
+++ src/main/java/com/codeup/springblog/controllers/BookController.java
@@ -1,8 +1,11 @@
 package com.codeup.springblog.controllers;
 
+import com.codeup.springblog.models.Book;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import repositories.BookRepository;
 
 @Controller
@@ -19,6 +22,19 @@ public class BookController {
         return "books";
     }
 
+    @GetMapping("/books/create")
+    public String createBook(Model model){
+        model.addAttribute("book", new Book());
+        return "books/create";
+    }
+
+    @PostMapping("/books/create")
+    public String createBook(@ModelAttribute Book book) {
+        //book.SetAuthor(authorsDao.getById(1L));
+        //books.save(book);
+        return "books/index";
+
+    }
 
 
 }
