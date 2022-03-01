71fcbea510090b85c944f9cd4f27ea9034498163
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-03-01 15:54:25 -0600

    services lectures

--- src/main/java/com/codeup/springblog/controllers/BookController.java
+++ src/main/java/com/codeup/springblog/controllers/BookController.java
@@ -1,20 +1,32 @@
 package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Book;
+import com.codeup.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import repositories.AuthorRepository;
 import repositories.BookRepository;
+import repositories.GenreRepository;
 
 @Controller
 public class BookController {
     private BookRepository bookDao;
+    private AuthorRepository authorsDao;
+    private GenreRepository genresDao;
+    private final EmailService emailService;
 
-    public BookController(BookRepository bookDao) {
+
+    public BookController(BookRepository bookDao, AuthorRepository authorsDao, GenreRepository genresDao, EmailService emailService) {
         this.bookDao = bookDao;
+        this.authorsDao = authorsDao;
+        this.genresDao = genresDao;
+        this.emailService = emailService;
     }
+
+
 
     @GetMapping("/books")
     public String showBooks(Model model) {
@@ -25,6 +37,7 @@ public class BookController {
     @GetMapping("/books/create")
     public String createBook(Model model){
         model.addAttribute("book", new Book());
+        model.addAttribute("genres", genresDao.findAll());
         return "books/create";
     }
 
@@ -32,8 +45,13 @@ public class BookController {
     public String createBook(@ModelAttribute Book book) {
         //book.SetAuthor(authorsDao.getById(1L));
         //books.save(book);
-        return "books/index";
+        return "redirect/index";
+    }
 
+    @GetMapping("/send-email")
+    public String sendEmail() {
+        emailService.prepareAndSend("Testing", "Did this work");
+        return "redirect:/";
     }
 
 
--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -143,3 +143,90 @@ public class PostController {
         return "redirect:/posts";
     }
 }
+
+
+ // Instructor's own solution
+
+//
+//package com.codeup.regulusspringblog.controllers;
+//
+//        import com.codeup.regulusspringblog.models.Post;
+//        import com.codeup.regulusspringblog.repositories.PostRepository;
+//        import com.codeup.regulusspringblog.repositories.UserRepository;
+//        import org.springframework.stereotype.Controller;
+//        import org.springframework.ui.Model;
+//        import org.springframework.web.bind.annotation.*;
+//
+//        import java.util.ArrayList;
+//        import java.util.List;
+//
+//@Controller
+//public class PostController {
+//    private PostRepository postsDao;
+//    private UserRepository usersDao;
+//
+//    public PostController(PostRepository postsDao, UserRepository usersDao) {
+//        this.postsDao = postsDao;
+//        this.usersDao = usersDao;
+//    }
+//
+//    @GetMapping("/posts")
+//    public String viewPosts(Model model) {
+//        model.addAttribute("allPosts", postsDao.findAll());
+//
+//        return "posts/index";
+//    }
+//
+//
+//    @GetMapping("/posts/{id}")
+//    public String postDetails(@PathVariable long id, Model model) {
+//        model.addAttribute("singlePost", postsDao.getById(id));
+//        return "posts/show";
+//    }
+//
+//
+//    @GetMapping("/posts/create")
+//    public String showCreateForm(Model model) {
+//        model.addAttribute("newPost", new Post());
+//        return "posts/create";
+//    }
+//
+//
+//    @PostMapping("/posts/create")
+//    public String submitCreateForm(@ModelAttribute Post newPost) {
+//        //Post newPost = new Post(title, body);
+//        newPost.setUser(usersDao.getById(1L));
+//        postsDao.save(newPost);
+//
+//        return "redirect:/posts";
+//    }
+//
+//    @GetMapping("/posts/{id}/edit")
+//    public String showEditForm(@PathVariable long id, Model model) {
+//        Post posttoEdit = postsDao.getById(id);
+//        model.addAttribute("postToEdit", posttoEdit);
+//        return "posts/edit";
+//    }
+//    // We can access the values submitted from the form using our @RequestParam annotation
+//    @PostMapping("/posts/{id}/edit")
+//    public String submitEdit(@ModelAttribute Post postToEdit, @PathVariable long id) {
+//
+//        // grab the post from our DAO
+////    Post postToEdit = postsDao.getById(id);
+//        // use setters to set new values to the object
+////    postToEdit.setTitle(title);
+////    postToEdit.setBody(body);
+//        // save the object with new values
+//        postsDao.save(postToEdit);
+//        return "redirect:/posts";
+//    }
+//
+//    // For now, we need to use a GetMapping, that way, when we visit the page,
+//    // our app can access the path variable, then delete the post, then redirect
+//    // us back to the post index page.
+//    @GetMapping("/posts/{id}/delete")
+//    public String delete(@PathVariable long id) {
+//        postsDao.deleteById(id);
+//        return "redirect:/posts";
+//    }
+//}
